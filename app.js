44fecb6dce1e0ae60edc126744fe98f62292fbb9
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-03-27 16:21:38 -0300

    refactor: separeted code into functions (#3)

--- app.js
+++ app.js
@@ -1,123 +1,56 @@
-const fs = require('fs');
-const XLSX = require('xlsx');
+const fs = require('fs')
+const XLSX = require('xlsx')
+// local imports
+const regexForStringSearch = require('./regexForStringSearch')
+const createSheet = require('./createSheet')
+const appendSheetsToWorkbook= require('./appendSheetsToWorkbook')
 
-
+//////////////////////////////////////////////////
 // Carrega o arquivo Excel
+const planilha = XLSX.readFile('sheet.xlsx');
 
-const workbook = XLSX.readFile('sheet.xlsx');
-
-// Obtém a aba desejada
-const sheetName = workbook.SheetNames[0];
-const workSheet = workbook.Sheets[sheetName];
-// Converte as células em JSON
-const dadosJSON = XLSX.utils.sheet_to_json(workSheet);
-
-// Escreve o arquivo JSON
-fs.writeFileSync('dados.json', JSON.stringify(dadosJSON));
-
+// Obtém a tabela desejada
+const nomeDaTabela = planilha.SheetNames[0];
+const tabela = planilha.Sheets[nomeDaTabela];
+// Converte a tabela em JSON
+const dadosJSON = XLSX.utils.sheet_to_json(tabela);
 
 //==========================================================//
-  let incorporadora = []
-  let construtora = []
-  let fundo = []
-  let investidor = []
-  let banco = []
-  let industria = []
-  let empresa = [] 
+let sortedArr = []
 
-const mycontact = {
-  keywords: ['incorporadora','construtora','fundo','investidor','banco','industria','empresa',],
-  
+const keywords = ['incorporadora','construtora','fundo','investidor','banco','industria','empresa',]
 
-  regexForStringSearch(query) {
-    query = query
-    .replace(/a/gi, '[AÁÀÂÃ]')
-      .replace(/e/gi, '[EÉÈÊ]')
-      .replace(/i/gi, '[IÍÌÎ]')
-      .replace(/o/gi, '[OÓÒÔÕ]')
-      .replace(/u/gi, '[UÚÙÛ]')
-      .replace(/c/gi, '[CÇ]')
-      return new RegExp(query, 'gmi')
-    },
-    
-    combinedName(){
-      // combinar os sheetNames
-      dadosJSON.forEach((contact) => {
-        const combineName = [contact.name,contact.name2,contact.name3 ].join('')
-        this.keywords.forEach(keyword => {
-        if(combineName.match(this.regexForStringSearch(keyword))){
+function basicParseSheet(){
+  // combinar os nomeDaTabelas
+  dadosJSON.forEach((contact) => {
+      const combineName = [contact.name,contact.name2,contact.name3 ].join('')
+      keywords.forEach(keyword => {
+        if(combineName.match(regexForStringSearch(keyword))){
           const formatedContact = {
             nome:combineName,
+            ramo: keyword,
             email:contact.Email,
             emailAlt:contact.emailAlt,
             phone1:contact.phone1,
             phone2:contact.phone2,
           }
 
-          switch(keyword) {
-            case "incorporadora":
-              incorporadora.push(formatedContact);
-              break;
-            case "construtora":
-              construtora.push(formatedContact);
-              break;
-            case "fundo":
-              fundo.push(formatedContact);
-              break;
-            case "investidor":
-              investidor.push(formatedContact);
-              break;
-            case "banco":
-              banco.push(formatedContact);
-              break;
-            case "industria":
-              industria.push(formatedContact);
-              break;
-            case "empresa":
-              empresa.push(formatedContact);
-              break;
-            default:
-              break
-          }        
+          sortedArr.push(formatedContact)
         }
       })
-    }
-    )
- },
-}
-// Adicione seus objetos de contato aos arrays aqui
-
-function createSheet(sheetName, data) {
-  const ws = XLSX.utils.json_to_sheet(data);
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, sheetName);
-  return wb;
+  }
+  )
 }
 
-function appendSheetsToWorkbook(workbook, sheets) {
-  sheets.forEach(({ sheetName, data }) => {
-    const ws = XLSX.utils.json_to_sheet(data);
-    XLSX.utils.book_append_sheet(workbook, ws, sheetName);
-  });
-  return workbook;
-}
+// Filtra os dados
+basicParseSheet()
+const sheet = { nomeDaTabela: 'Filtrada', data: sortedArr }
+createSheet(sheet.nomeDaTabela, sheet.data)
 
-const sheets = [
-  { sheetName: 'Incorporadora', data: incorporadora },
-  { sheetName: 'Construtora', data: construtora },
-  { sheetName: 'Fundo', data: fundo },
-  { sheetName: 'Investidor', data: investidor },
-  { sheetName: 'Banco', data: banco },
-  { sheetName: 'Industria', data: industria },
-  { sheetName: 'Empresa', data: empresa },
-];
-
-mycontact.combinedName()
+// Monta noco arquivo Excel
 const wb = XLSX.utils.book_new();
-appendSheetsToWorkbook(wb, sheets);
-
-// Substitua 'output.xlsx' pelo nome desejado do arquivo
+appendSheetsToplanlha(wb, sheet);
 XLSX.writeFile(wb, 'output.xlsx');
 
-createSheet(`batata`,fundo)
-console.log(incorporadora)
+
+console.log(sortedArr)
---
+++ createSheet.js
@@ -0,0 +1,7 @@
+function createSheet(sheetName, data) {
+    const ws = XLSX.utils.json_to_sheet(data);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, sheetName);
+    return wb;
+  }
+module.exports = createSheet;
